ab418fa03246debbf4d292edc0da7a47537e81a5
LingCheng, Kuo <darrenkuo0413@gmail.com>
2019-08-28 16:30:47 +0800

    Create Plot_Image_for_Model.py

---
+++ GAN/GAN/Plot_Image_for_Model.py
@@ -0,0 +1,24 @@
+import numpy as np
+from tensorflow.keras import models
+
+G = models.load_model('generator_model.h5')
+
+width = 28
+height = 28
+z_dim = 100
+
+
+def Plot_Generated(n_ex = 5, dim = (1, 5), figsize = (12, 2)):
+    noise = np.random.normal(0, 1, size = (n_ex, z_dim))
+    generated_images = G.predict(noise)
+    generated_images = generated_images.reshape(generated_images.shape[0], width, height)
+    plt.figure(figsize = figsize)
+    for i in range(generated_images.shape[0]):
+        plt.subplot(dim[0], dim[1], i+1)
+        plt.imshow(generated_images[i, :, :], interpolation = 'nearest', cmap = 'gray_r')
+        plt.axis('off')
+    plt.tight_layout()
+    plt.show()
+    
+    
+Plot_Generated()
